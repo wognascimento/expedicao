da2109e6a8026ff384c3782430c126b0dcc67647
wesley <wog.nascimento@gmail.com>
2024-10-22 18:09:10 -0300

    ATUALIZAÇÃO DE DEPENDENCIAS

--- Expedicao/App.xaml.cs
+++ Expedicao/App.xaml.cs
@@ -10,8 +10,8 @@ namespace Expedicao
     {
         public App()
         {
-            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjQyN0AzMjMwMkUzMzJFMzBPcFQ2bVNrT1RpOWNSQlo5cDduRG83STVVUlhGcmcrNnRKOU9GTk5JV2o0PQ==");
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM5MkAzMjM0MkUzMTJFMzlZRnNmeEdKa0haRGU0S0MyZUR3b05vcDJFNURBbnFRTi9STUVidExydWswPQ==");
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM5MkAzMjM0MkUzMTJFMzlZRnNmeEdKa0haRGU0S0MyZUR3b05vcDJFNURBbnFRTi9STUVidExydWswPQ==");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTU4NUAzMjM3MkUzMTJFMzluT08wbzRnYm4zUlFDOVRzWVpYbUtuSEl0aUhTZmNMYjQxekhrV0NVRnlzPQ==");
 
             DataBase BaseSettings = DataBase.Instance;
             BaseSettings.Database = DateTime.Now.Year.ToString();
