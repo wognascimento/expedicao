4d90d9dd8dd72f9867f6d7406465ea0b140d2959
wesley <wog.nascimento@gmail.com>
2025-04-01 12:20:55 -0300

    APLICADO NOVO METODO DE ATUALIZAÇÃO

--- Expedicao/App.xaml.cs
+++ Expedicao/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BibliotecasSIG;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Expedicao
@@ -8,6 +14,10 @@ namespace Expedicao
     /// </summary>
     public partial class App : Application
     {
+
+        private const string UPDATE_URL = "http://192.168.0.49/downloads/expedicao/version.json";
+        private readonly string CURRENT_VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
         public App()
         {
             //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM5MkAzMjM0MkUzMTJFMzlZRnNmeEdKa0haRGU0S0MyZUR3b05vcDJFNURBbnFRTi9STUVidExydWswPQ==");
@@ -18,6 +28,86 @@ namespace Expedicao
             BaseSettings.Host = "192.168.0.23";
             BaseSettings.Username = Environment.UserName;
             BaseSettings.Password = "123mudar";
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            /*
+            CultureInfo culture = new("pt-BR");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(culture.IetfLanguageTag)));
+
+            */
+            // Verificação de atualização em segundo plano
+            await CheckForUpdatesAsync();
+        }
+        private async Task CheckForUpdatesAsync()
+        {
+            try
+            {
+                var updateChecker = new UpdateChecker(UPDATE_URL, CURRENT_VERSION);
+                var updateInfo = await updateChecker.CheckForUpdatesAsync();
+
+                var updateInfoJson = JsonSerializer.Serialize<UpdateInfo>(updateInfo);
+
+                if (updateInfo != null)
+                {
+                    // Pergunta ao usuário se deseja atualizar
+                    var result = MessageBox.Show(
+                        $"Nova versão disponível!\n\n" +
+                        $"Versão atual: {CURRENT_VERSION}\n" +
+                        $"Nova versão: {updateInfo.updateVersion}\n\n" +
+                        "Changelog:\n" +
+                        string.Join("\n", updateInfo.changelog) +
+                        "\n\nDeseja baixar a atualização?",
+                        "Atualização Disponível",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Information
+                    );
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+
+                        var options = new JsonSerializerOptions { WriteIndented = true };
+                        string jsonString = JsonSerializer.Serialize(updateInfo, options);
+
+                        //Process.Start("Update.exe", @$"{updateInfoJson}, CentralSIG.exe");
+
+                        string jsonData = JsonSerializer.Serialize(updateInfo); // Garante que o JSON está bem formatado
+                        string appName = "CentralSIG.exe";
+
+                        string arguments = $"\"{jsonData.Replace("\"", "\\\"")}\" \"{appName}\"";
+                        Process.Start("Update.exe", arguments);
+                        this.Shutdown();
+
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Log do erro ou tratamento de exceção
+                MessageBox.Show(
+                    $"Erro ao verificar atualizações: {ex.Message}",
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Erro ao verificar atualizações: {ex.Message}",
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
         }
     }
 }
