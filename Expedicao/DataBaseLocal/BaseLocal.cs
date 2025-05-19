85b4bf7406b19f7890ecbeb908c9a05a8a79ac29
wesley <wog.nascimento@gmail.com>
2025-05-19 12:21:42 -0300

    ALTERAR CAMINHO BANCO DE DADOS LOCAL

--- Expedicao/DataBaseLocal/BaseLocal.cs
+++ Expedicao/DataBaseLocal/BaseLocal.cs
@@ -3,13 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Expedicao
 {
-    internal class BaseLocal : DbContext
+    public class BaseLocal : DbContext
     {
+        DataBase BaseSettings = DataBase.Instance;
+
         public virtual DbSet<ItemCarregado> ItemCarregados { get; set; }
         public virtual DbSet<ItemFaltantes> ItemFaltantes { get; set; }
         public virtual DbSet<Romaneio> Romaneios { get; set; }
         public virtual DbSet<RomaneioCaminhao> RomaneioCaminhaos { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("Data Source=Database.db");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(@$"Data Source={BaseSettings.CaminhoSistema}\Database.db");
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<RomaneioCaminhao>().HasKey(m => new {m.id_aprovado, m.numero_caminhao});
